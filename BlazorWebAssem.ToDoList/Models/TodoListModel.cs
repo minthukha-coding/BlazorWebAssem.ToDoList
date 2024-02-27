622d4e35e70a814730792e9d6f1dbe9479e9ee51
minthukha-coding <minthukha1239788@gmail.com>
2024-02-27 13:43:06 +0630

    Add project files.

---
+++ BlazorWebAssem.ToDoList/Models/TodoListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebAssem.ToDoList.Models
+{
+    public class TodoListModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public bool IsCompleted { get; set; }
+        public string TaskName { get; set; }
+    }
+}
---
+++ BlazorWebAssem.ToDoList/Pages/Todo.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWebAssem.ToDoList.Models;
+
+namespace BlazorWebAssem.ToDoList.Pages
+{
+    public partial class Todo
+    {
+        public static List<TodoListModel> List { get; set; } = new List<TodoListModel>();
+        public TodoListModel reqModel { get; set; } = new TodoListModel();
+
+        private void Add()
+        {
+            if (reqModel == null)
+            {
+                return;
+            }
+            List.Add(reqModel);
+            reqModel = new TodoListModel();
+        }
+    }
+}
